531d17a0179d4fc88571028f51d7b5324e452c76
KashifHaider8105 <146319132+KashifHaider8105@users.noreply.github.com>
2024-01-10 15:19:20 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Web_Programming_03
+# Codes-Wear
+
+<img width="100%" src="/public/codeswear.jpg" alt="Codes-Wear - Thumbnail" />
+
+### Project Created at
+- 🗓 *Dec 2023 - Jamuary 2024*
+
+### Technologies Used ⚒️
+- Next.js
+- Tailwindcss
+- MongoDB & Mongoose
+- Jsonwebtoken
+
+### Features
+- 👥 Create & Login account
+- 🔑 Forgot password functionality
+- 🔒 Change password securely
+- 🛒 Add items into the cart
+- 💳 Seamless checkout process
+- 📦 View & Manage orders
+- 🚪 Logout functionality 
+
+### Live Demo 🎉
+Explore the live version of Codes-Wear <a target="_blank" href="">here</a>.
+
+### .env Setup
+```sh
+ADMIN_USER=<required for admin panel login>
+ADMIN_PASS=<required for admin panel login>
+MONGO_URI=<your mongo db uri without db_name>
+SECRET_KEY=<required to sign jwt>
+EMAIL_USER=<your gmail username to send account confirmation email or other email>
+EMAIL_PASS=<your gmail password to send account confirmation email or other email>
+```
+
+### 📬 Connect with me
+> Let's connect! Reach out for collaborations, projects, or just a friendly chat.
+Contact Number:03341644377
---
+++ pages/about.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2 className="mt-5 text-center text-3xl font-semibold">About Us</h2>
+      <div className="my-2 px-5 mb-4">
+        <p className="font-light text-center text-md">
+          Welcome to our platform! We are dedicated to providing you with a unique and enjoyable online shopping experience. Our mission is to offer high-quality products with a seamless and secure shopping process.
+
+          At our core, we value customer satisfaction and strive to exceed your expectations. Our team is committed to curating a diverse range of products to cater to your needs. We believe in transparency, excellent customer service, and delivering products that bring joy and satisfaction to your life.
+
+          Whether you're looking for the latest fashion trends, technological gadgets, or home essentials, we've got you covered. Explore our website, discover exciting deals, and shop with confidence.
+
+          Thank you for choosing us. We look forward to serving you and making your online shopping journey memorable.
+
+          If you have any questions or feedback, feel free to reach out. Happy shopping!
+        </p>
+        <br />
+        <p className="mt-2 font-light text-center text-md">
+          Explore our collections and discover the perfect items for your lifestyle.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
